3c0ec1fa9f12b6a4f8ea638afab067f158aa9a82
nghib20 <nghib20@freeuni.edu.ge>
2024-05-02 15:53:31 +0400

    Add project files.

---
+++ NumGuesser/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace MyNamespace
+{
+    class NumGuesser
+    {
+        public static void Main(string[] args)
+        {
+            NumGuesser numGuesser = new NumGuesser();
+            numGuesser.GuessNumber();
+        }
+
+        public void GuessNumber()
+        {
+            Console.Write("Enter upper limit: ");
+            int maxNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter lower limit: ");
+            int minNumber = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                int guess = (maxNumber + minNumber) / 2;
+                Console.WriteLine("Did you guess " + guess + "?");
+                string input = Console.ReadLine();
+                if (input.Equals("yes")) {
+                    Console.WriteLine("Your guess was " + guess);
+                    break;
+                } else if (input.Equals(">"))
+                {
+                    minNumber = guess;
+                } else if (input.Equals("<"))
+                {
+                    maxNumber = guess;
+                } else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+        }
+    }
+}
